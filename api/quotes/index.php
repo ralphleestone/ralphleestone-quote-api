672a523350107ef0db1c3431020f78d08ed80841
Ralph Lee Stone <ralphleestone@gmail.com>
2022-03-14 19:33:07 -0600

    added to index files

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,1 +1,7 @@
-<h1>Loaded</h1>
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+}
