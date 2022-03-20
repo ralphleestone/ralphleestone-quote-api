53dfda4acc7bac4da4cba9688df9547e7171f3d9
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 16:51:50 -0600

    update

--- config/Database.php
+++ config/Database.php
@@ -4,9 +4,9 @@ class Database {
   public function connect() {
     $url = getenv('JAWSDB_URL');
     $dbparts = parse_url($url);
-    $hostname = $dbparts['exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
-    $username = $dbparts['kpyim9nuco43e7mw'];
-    $password = $dbparts['t8865p922fmp9a7u'];
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
     $database = ltrim($dbparts['path'],'/');
     try {
       $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
