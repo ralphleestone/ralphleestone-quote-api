994f251b57db1339e1aea922bff3cd103b675b59
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 01:41:26 -0600

    load qoutes

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,13 +1,38 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-$method = $_SERVER['REQUEST_METHOD'];
-
-if ($method === 'OPTIONS') {
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-}
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
 $database = new Database();
 $db = $database->connect();
+$quote = new Quote($db);
+$result = $quote->read();
+$num = $result->rowCount();
+
+if($num > 0) {
+    $quote_arr = array();
+    $quote_arr = array();
+    
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $quote_item = array( 
+            'id' => $id,
+            'quote' => html_entity_decode($quote),
+            'author' => $author,
+            'category' => $category
+        );
+        
+        array_push($quote_arr, $quote_item);
+    }
+    echo(json_encode($quote_arr));
+} else {
+    echo json_encode(
+        array('message' => 'No quotes found')
+    );
+}
+
+?>
--- models/Quote.php
+++ models/Quote.php
@@ -1,5 +1,85 @@
 <?php
 
 class Quote {
+    private $conn;
+    private $table = 'quotes';
+    public $id;
+    public $quote;
+    public $authorId;
+    public $categoryId;
     
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+    
+    public function read() {
+        $query = 'SELECT q.id, q.quote, a.author, c.category From' . $this->table . ' q LEFT JOIN authors a ON q.authorId = a.id LEFT JOIN category c ON q.categoryId = c.id';
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+    public function read_single() {
+        $query = 'SELECT q.id, q.quote, a.author, c.category From' . $this->table . ' q LEFT JOIN authors a ON q.authorId = a.id LEFT JOIN category c ON q.categoryId = c.id WHERE  q.id = :id';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $this->id);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->id = $row['id'];
+        $this->quote = $row['quote'];
+        $this->author = $row['author'];
+        $this->category = $row['category'];
+    }
+    
+    public function create() {
+        $query = 'INSERT INTO ' . $this->table . 'SET id = :id, quote = :quote, authorId = :authorId, categoryId = :categoryId';
+        $stmt = $this->conn->prepare($query);
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->quote = htmlspecialchars(strip_tags($this->quote));
+        $this->authorId = htmlspecialchars(strip_tags($this->authorId));
+        $this->categoryId = htmlspecialchars(strip_tags($this->categoryId));
+        
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':quote', $this->quote);
+        $stmt->bindParam(':authorId', $this->authorId);
+        $stmt->bindParam(':categoryId', $this->categoryId);
+        
+        if($stmt->execute()) {
+            return true;
+        } 
+        echo("Error: %s. \n", $stmt->error);
+        return false;
+    }
+    
+    public function update() {
+        $query = 'UPDATE ' . $this->table . 'SET id = :id, quote = :quote, authorId = :authorId, categoryId = :categoryId WHERE id = :id';
+        $stmt = $this->conn->prepare($query);
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->quote = htmlspecialchars(strip_tags($this->quote));
+        $this->authorId = htmlspecialchars(strip_tags($this->authorId));
+        $this->categoryId = htmlspecialchars(strip_tags($this->categoryId));
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':quote', $this->quote);
+        $stmt->bindParam(':authorId', $this->authorId);
+        $stmt->bindParam(':categoryId', $this->categoryId);
+        
+        if($stmt->execute()) {
+            return true;
+        } 
+        echo("Error: %s. \n", $stmt->error);
+        return false;
+    }
+    
+    public function delete() {
+        $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+        $stmt = $this->conn->prepare($query);
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $stmt->bindParam(':id', $this->id);
+        if($stmt->execute()) {
+            return true;
+        } 
+        echo("Error: %s. \n", $stmt->error);
+        return false;
+    }
 }
+
+?>
