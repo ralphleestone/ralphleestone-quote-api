7abb2efa523bf8221fc0d5b24a46764b8ccd8f46
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 15:17:09 -0600

    update

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -3,51 +3,39 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     $method = $_SERVER['REQUEST_METHOD'];
+    
     if ($method === 'OPTIONS') {
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
         header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     }
-
+    
     $isAnId = filter_input(INPUT_GET, "id");
     $isAnAuthorId = filter_input(INPUT_GET, "authorId");
     $isAnCategoryId = filter_input(INPUT_GET, "categoryId");
     
-
-   if(!empty($isAnId) && $method == 'GET') {
-     
-    include('./read_single.php');
+    if(!empty($isAnId) && $method == 'GET') {
+        include('./read_single.php');
     }
-
     else if (!empty($isAnCategoryId) && !empty($isAnAuthorId) && $method == 'GET') {
         include('./By_CategoryId_And_AuthorId.php');
     }
-
     else if (!empty($isAnAuthorId) && $method == 'GET') {
-        
-        include('./By_AuthorID.php');
+        include('./By_AuthorId.php');
     }
-
     else if (!empty($isAnCategoryId) && $method == 'GET') {
-      
         include('./By_CategoryId.php');
     }
-
     else if ($method == 'POST') {
         include('./create.php');
     }
-
     else if ($method == 'PUT') {
         include('./update.php');
     }
-
     else if ($method == 'DELETE') {
         include('./delete.php');
     }
-
-    
-
-else if($method == 'GET') {
-    include('./quotes.php');
-} 
+    else if($method == 'GET') {
+        include('./quotes.php');
+    } 
 
 ?>
