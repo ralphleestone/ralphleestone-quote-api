bef7bd39662f0183affecc3fdafaefd399b57b11
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 02:24:03 -0600

    update

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -15,24 +15,6 @@ $isAnCategoryId = filter_input(INPUT_GET, "categoryId");
 if(!empty($isAnId) && $method == 'GET') {
     include('./quotes_read_single.php');
 }
-else if (!empty($isAnCategoryId) && !empty($isAnAuthorId) && $method == 'GET') {
-    include('./quoteByCategoryIdAndAuthorId.php');
-}
-else if (!empty($isAnAuthorId) && $method == 'GET') {
-    include('./quoteByAuthorID.php');
-}
-else if (!empty($isAnCategoryId) && $method == 'GET') {
-    include('./quoteByCategoryId.php');
-}
-else if ($method == 'POST') {
-    include('./createQuote.php');
-}
-else if ($method == 'PUT') {
-    include('./updateQuote.php');
-}
-else if ($method == 'DELETE') {
-    include('./deleteQuote.php');
-}
 else if($method == 'GET') {
     include('./quotes.php');
 } 
