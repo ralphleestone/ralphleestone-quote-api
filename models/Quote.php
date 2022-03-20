3de22267fb273f1ee040230d186532f97bfc2ced
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 00:22:20 -0600

    format update

--- models/Quote.php
+++ models/Quote.php
@@ -1,30 +1,5 @@
 <?php
-class Quote {    
+
+class Quote {
     
-    private $conn;
-    private $table = 'kgk3wzi0cz11l61j.quotes';
-
-    // Post Properties
-    public $id;
-    public $category_id;
-    public $author;
-
-    // Constructor with DB
-    public function __construct($db) {
-      $this->conn = $db;
-    }
-
-    // Get categories
-        public function read() {
-            // Create query
-            $query = 'SELECT id, name, created_at FROM' . $this->table;
-      
-            // Prepare statement
-            $stmt = $this->conn->prepare($query);
-      
-            // Execute query
-            $stmt->execute();
-      
-            return $stmt;
-          }
 }
