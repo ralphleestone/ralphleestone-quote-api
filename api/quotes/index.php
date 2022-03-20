e47adc1fd82ef50205d901a6e20720e6efc50a9e
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 10:30:09 -0600

    update

---
+++ api/quotes/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Origin,Content-Type, Access-Control-Allow-Methods,Authorization, X-Requested-With');
+
+$database = new Database();
+$db = $database->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$isAnId = filter_input(INPUT_GET, "id");
+$isAnAuthorId = filter_input(INPUT_GET, "authorId");
+$isAnCategoryId = filter_input(INPUT_GET, "categoryId");
+
+if(!empty($isAnId) && $method == 'GET') {
+    include('./quotes_read_single.php');
+}
+else if($method == 'GET') {
+    include('./quotes.php');
+} 
+
+?>
