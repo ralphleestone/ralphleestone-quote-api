956769c6c794d0b530ab9c764f06a2df9736e855
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 02:09:35 -0600

    update

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,38 +1,40 @@
-<?php
+<?php 
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Origin,Content-Type, Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
-include_once '../../config/Database.php';
-include_once '../../models/Quote.php';
+$method = $_SERVER['REQUEST_METHOD'];
+$isAnId = filter_input(INPUT_GET, "id");
+$isAnAuthorId = filter_input(INPUT_GET, "authorId");
+$isAnCategoryId = filter_input(INPUT_GET, "categoryId");
 
-$database = new Database();
-$db = $database->connect();
-$quote = new Quote($db);
-$result = $quote->read();
-$num = $result->rowCount();
-
-if($num > 0) {
-    $quote_arr = array();
-    $quote_arr = array();
-    
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
-
-        $quote_item = array( 
-            'id' => $id,
-            'quote' => html_entity_decode($quote),
-            'author' => $author,
-            'category' => $category
-        );
-        
-        array_push($quote_arr, $quote_item);
-    }
-    echo(json_encode($quote_arr));
-} else {
-    echo json_encode(
-        array('message' => 'No quotes found')
-    );
+if(!empty($isAnId) && $method == 'GET') {
+    include('./quotes_read_single.php');
 }
+else if (!empty($isAnCategoryId) && !empty($isAnAuthorId) && $method == 'GET') {
+    include('./quoteByCategoryIdAndAuthorId.php');
+}
+else if (!empty($isAnAuthorId) && $method == 'GET') {
+    include('./quoteByAuthorID.php');
+}
+else if (!empty($isAnCategoryId) && $method == 'GET') {
+    include('./quoteByCategoryId.php');
+}
+else if ($method == 'POST') {
+    include('./createQuote.php');
+}
+else if ($method == 'PUT') {
+    include('./updateQuote.php');
+}
+else if ($method == 'DELETE') {
+    include('./deleteQuote.php');
+}
+else if($method == 'GET') {
+    include('./quotes.php');
+} 
 
 ?>
