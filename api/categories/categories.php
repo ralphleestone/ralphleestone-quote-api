1128d347960bbe7b193eba17206ff499941dfe4a
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 18:54:22 -0600

    updates

---
+++ api/categories/categories.php
@@ -0,0 +1,46 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+
+
+$database = new Database();
+$db = $database->connect();
+
+$category = new Category($db);
+
+$result = $category->read();
+
+$num = $result->rowCount();
+
+if($num > 0) {
+    // make an array of the quotes
+    $category_arr = array();
+    $category_arr = array();
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $category_item = array( 
+            'id' => $id,
+            'category' => $category   
+        );
+
+        array_push($category_arr, $category_item);
+    
+    }
+
+    print_r(json_encode($category_arr));
+
+} else {
+    echo json_encode(
+        array('message' => 'No categories found')
+    );
+}
+
+
+?>
