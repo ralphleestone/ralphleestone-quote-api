261dc17069bda4a94532d226c1e4155d29e1a770
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 19:39:26 -0600

    updates

--- api/authors/update.php
+++ api/authors/update.php
@@ -12,9 +12,7 @@ include_once '../../models/Author.php';
 $database = new Database();
 $db = $database->connect();
 
-
 $author = new Author($db);
-
 
 $data = json_decode(file_get_contents("php://input"));
 
