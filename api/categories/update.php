7818144750bdf3e54822ed9501e63e540aadb375
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 18:56:41 -0600

    updates

--- api/categories/update.php
+++ api/categories/update.php
@@ -1,13 +1,13 @@
 <?php
 
 include_once '../../config/Database.php';
-include_once '../../models/Categories.php';
+include_once '../../models/Category.php';
 
 
 $database = new Database();
 $db = $database->connect();
 
-$category = new Categories($db);
+$category = new Category($db);
 
 
 $data = json_decode(file_get_contents("php://input"));
