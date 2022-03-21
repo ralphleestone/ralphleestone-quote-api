5fbf1af0919a7dc7ce3cb41a1aca366b0bf3c4fd
=Ralph Stone <ralphleestone@gmail.com>
2022-03-21 17:13:31 -0600

    update

--- api/IsValid/IsValid.php
+++ api/IsValid/IsValid.php
@@ -1,4 +1,9 @@
 <?php
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
+include_once '../../models/Author.php';
+include_once '../../models/Category.php';
+
 function isValid($id, $model) {
     $model->id = $id;
     $modelResult = $model->read_single();
