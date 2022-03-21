06e540fb7f30d9a9f7227eb4c465645ad479a712
=Ralph Stone <ralphleestone@gmail.com>
2022-03-21 16:06:20 -0600

    update

--- config/Database.php
+++ config/Database.php
@@ -10,7 +10,6 @@ class Database {
     try {
       $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      //echo "Connection Succeeded!";
     }catch(PDOException $e){
       echo "Connection failed: " . $e->getMessage();
     }
