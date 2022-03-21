79d01864f97eac386eba54e5c3797d55e94522ac
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 23:38:18 -0600

    update

--- api/categories/index.php
+++ api/categories/index.php
@@ -12,23 +12,18 @@ $isAnId = filter_input(INPUT_GET, "id");
 if(isset($isAnId) && $method == 'GET') {
     include('./read_single.php');
 }
-
 else if ($method == 'GET') {
     include('./categories.php');
 }
-
 else if ($method == 'POST') {
     include('./create.php');
 } 
-
 else if ($method == 'PUT') {
     include('./update.php');
 }
-
 else if ($method == 'DELETE') {
     include('./delete.php');
 }
-
 else {
     echo json_encode(
         array('message' => 'No Quotes Found')
