1c0ff1fd90bd5b1690894b3a8f48854169e68430
=Ralph Stone <ralphleestone@gmail.com>
2022-03-21 11:24:30 -0600

    update

--- api/categories/update.php
+++ api/categories/update.php
@@ -10,7 +10,7 @@ $category = new Category($db);
 $data = json_decode(file_get_contents("php://input"));
 
 $category->id = $data->id;
-//$category->category = $data->category;
+$category->category = $data->category;
 
 if($category->update()){
     echo json_encode(
