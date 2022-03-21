0e34dceec5db9c415ea5a32e7ab2e39e68b3b6b8
=Ralph Stone <ralphleestone@gmail.com>
2022-03-21 12:50:07 -0600

    update

--- api/categories/create.php
+++ api/categories/create.php
@@ -17,7 +17,7 @@ $data = json_decode(file_get_contents("php://input"));
 $category->id = $data->id;
 $category->category = $data->category;
 
- if($category->create() || isset($category->id)){
+ if($category->create()){
      
    echo json_encode(
         array(
