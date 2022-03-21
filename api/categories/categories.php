e94a91ecb3dda10fda570368d6c644c093710a3a
=Ralph Stone <ralphleestone@gmail.com>
2022-03-21 16:56:28 -0600

    updates

--- api/categories/categories.php
+++ api/categories/categories.php
@@ -16,6 +16,7 @@ $num = $result->rowCount();
 
 if($num > 0) {
     $category_arr = array();
+    $category_arr = array();
 
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
--- api/quotes/quotes.php
+++ api/quotes/quotes.php
@@ -15,6 +15,7 @@ $num = $result->rowCount();
 
 if($num > 0) {
     $quote_arr = array();
+    $quote_arr = array();
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
         $quote_item = array( 
