99ac3f8e65c60e29a693e21a3a05097f5943812e
=Ralph Stone <ralphleestone@gmail.com>
2022-03-21 15:33:16 -0600

    update

---
+++ api/IsValid/Is_Valid.php
@@ -0,0 +1,5 @@
+<?php
+function isValid($id,$model){
+
+}
+?>
--- api/quotes/update.php
+++ api/quotes/update.php
@@ -16,10 +16,7 @@ $quote = new Quote($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-//$quote->id = $data->id;
-if(isset($data->id) === true){
 $quote->id = $data->id;
-}
 $quote->quote = $data->quote;
 $quote->authorId = $data->authorId;
 $quote->categoryId = $data->categoryId;
