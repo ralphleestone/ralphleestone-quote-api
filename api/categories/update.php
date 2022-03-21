4c5398729e8a85a6264b8ea17ec29c7f9d6c4105
=Ralph Stone <ralphleestone@gmail.com>
2022-03-20 18:56:06 -0600

    updates

---
+++ api/categories/update.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once '../../config/Database.php';
+include_once '../../models/Categories.php';
+
+
+$database = new Database();
+$db = $database->connect();
+
+$category = new Categories($db);
+
+
+$data = json_decode(file_get_contents("php://input"));
+
+$category->id = $data->id;
+$category->category = $data->category;
+
+
+if($category->update()){
+    echo json_encode(
+        array('id' => $category->id,
+              'category' => $category->category)
+    );
+} else {
+    echo json_encode(
+        array('message' => 'category Not updated')
+    );
+}
